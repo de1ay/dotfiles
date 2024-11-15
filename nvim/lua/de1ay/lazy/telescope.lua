64050ee3ac7cb5e23d4c81e30547fa24f97462a8
Nikita Ivanov <github@de1ay.me>
2024-11-15 14:17:49 +0300

    +: telescope fzf and devicons

--- nvim/lua/de1ay/lazy/telescope.lua
+++ nvim/lua/de1ay/lazy/telescope.lua
@@ -1,22 +1,33 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.8",
+  tag = "0.1.8",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+
+      build = 'make',
+
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
     },
+    {
+      'nvim-tree/nvim-web-devicons',
+      enabled = vim.g.have_nerd_font
+    }
+  },
 
-    config = function()
-        require('telescope').setup({})
+  config = function()
+    require('telescope').setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = "[S]earch [G]it files" })
-        vim.keymap.set('n', '<leader>ss', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end, { desc = "[S]earch [S]tring" })
-        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "[S]earch [H]elp" })
-    end
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = "[S]earch [F]iles" })
+    vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = "[S]earch [G]it files" })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = "[S]earch current [W]ord" })
+    vim.keymap.set('n', '<leader>ss', builtin.live_grep, { desc = "[S]earch [S]tring" })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "[S]earch [H]elp" })
+  end
 }
-
