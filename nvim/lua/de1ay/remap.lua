76f1e0383397c3c6d9b4b62e0bb0226228015421
Nikita Ivanov <github@de1ay.me>
2025-01-25 21:38:10 +0300

    feat(nvim-remap): add remap for pasting without overwriting of register

--- nvim/lua/de1ay/remap.lua
+++ nvim/lua/de1ay/remap.lua
@@ -20,6 +20,9 @@ vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
 -- Delete without rewriting registers
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
+-- Paste without rewriting registers
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_dP")
+
 -- Move selected lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
