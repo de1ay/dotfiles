37109ba1a3c7da74f929bd96699408d90da34368
Nikita Ivanov <github@de1ay.me>
2025-01-25 21:37:20 +0300

    feat(nvim-telescope): add diagnostics search

--- nvim/lua/de1ay/lazy/telescope.lua
+++ nvim/lua/de1ay/lazy/telescope.lua
@@ -1,35 +1,36 @@
 return {
-  "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-  tag = "0.1.8",
+	tag = "0.1.8",
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
 
-      build = "make",
+			build = "make",
 
-      cond = function()
-        return vim.fn.executable "make" == 1
-      end,
-    },
-    {
-      "nvim-tree/nvim-web-devicons",
-      enabled = vim.g.have_nerd_font
-    }
-  },
+			cond = function()
+				return vim.fn.executable "make" == 1
+			end,
+		},
+		{
+			"nvim-tree/nvim-web-devicons",
+			enabled = vim.g.have_nerd_font
+		}
+	},
 
-  config = function()
-    require("telescope").setup({})
+	config = function()
+		require("telescope").setup({})
 
-    require("telescope").load_extension("fzf")
+		require("telescope").load_extension("fzf")
 
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "[S]earch [G]it files" })
-    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>ss", builtin.live_grep, { desc = "[S]earch [S]tring" })
-    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-  end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "[S]earch [G]it files" })
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+		vim.keymap.set("n", "<leader>ss", builtin.live_grep, { desc = "[S]earch [S]tring" })
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+	end
 }
