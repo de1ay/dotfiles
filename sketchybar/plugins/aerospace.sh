bc52b4628ec1bc2c2f444420711c304fcf7d67ea
Nikita Ivanov <github@de1ay.me>
2025-05-25 03:10:54 +0300

    feat(skethybar-aerospace): change bg colour instead of turning it on/off

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set "$NAME" background.drawing=on
+    sketchybar --set "$NAME" background.color=0x44ffffff
 else
-    sketchybar --set "$NAME" background.drawing=off
+    sketchybar --set "$NAME" background.color=0x00000000
 fi
 
