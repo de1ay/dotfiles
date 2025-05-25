79aa1eba2b7bc8a7f26c2a80828b37be2b0705eb
Nikita Ivanov <github@de1ay.me>
2025-05-25 22:11:36 +0300

    fix(sketchybar-apptime): add endline to grep query for bundleIdentifier

--- sketchybar/plugins/apptime.sh
+++ sketchybar/plugins/apptime.sh
@@ -3,10 +3,10 @@
 if [ "$SENDER" = "front_app_switched" ]; then
 	APP_NAME=$INFO
 else
-	APP_NAME=$(sketchybar --query front_app | jq .label.value | tr -d "\"" )
+	APP_NAME=$(sketchybar --query front_app | jq .label.value | tr -d "\"")
 fi
 
-BUNDLE_IDENTIFIER=$(aerospace list-apps | grep "$APP_NAME" | awk '{print $3}')
+BUNDLE_IDENTIFIER=$(aerospace list-apps | grep "$APP_NAME$" | awk '{print $3}')
 APPTIME=$(curl 127.0.0.1:5501/app/$BUNDLE_IDENTIFIER)
 
 sketchybar --set "$NAME" label="$APPTIME"
