842ac8895b0aa8e6b911e8228bce55db0382dbf4
Nikita Ivanov <git@de1ay.me>
2025-07-31 03:58:14 +0300

    chore(nvim): replace deprecated config options with modern ones

--- nvim/lua/de1ay/set.lua
+++ nvim/lua/de1ay/set.lua
@@ -1,35 +1,35 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.opt.guicursor = ""
+vim.o.guicursor = ""
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
-vim.opt.smartindent = true
+vim.o.smartindent = true
 
-vim.opt.wrap = false
+vim.o.wrap = false
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
 
-vim.opt.inccommand = "split"
+vim.o.inccommand = "split"
 
-vim.opt.cursorline = true
+vim.o.cursorline = true
 
-vim.opt.signcolumn = "yes"
+vim.o.signcolumn = "yes"
 
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
-vim.opt.scrolloff = 10
+vim.o.scrolloff = 10
 
-vim.opt.updatetime = 50
+vim.o.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.o.colorcolumn = "80"
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
